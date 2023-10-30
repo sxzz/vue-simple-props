2eca6d5c116a0c8efd7c4d06207f82a4393813ee
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-10-31 01:46:36 +0800

    refactor: custom conditions for dev

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -4,5 +4,8 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    conditions: ['dev'],
+  },
   plugins: [vue(), vueJsx()],
 })
