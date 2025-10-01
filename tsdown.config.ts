72c4be50fc35cacefd1bd5e13aeb8c3aaadba6ca
Kevin Deng <sxzz@sxzz.moe>
2025-10-01 11:06:23 +0800

    ci: publish on CI

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'tsdown'
 
 export default defineConfig({
-  entry: './src/index.ts',
   target: 'es2021',
   platform: 'neutral',
+  exports: {
+    devExports: 'dev',
+  },
 })
